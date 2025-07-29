a2eaf66f7cd5ba6b309e4e3de966504e00f03355
way2masoom <way2masoom@gmail.com>
2025-07-29 11:11:38 +0545

    Variable Naming

---
+++ WebDev/JavaScript/01_BasicOfJs/DataTypeDemo.js
@@ -0,0 +1,10 @@
+// Product demo
+
+let productPrice=1298888;
+let DiscountAmount=15;
+
+let ProductName="Iphone 17 Pro max(256 Gb Black )"
+
+let isAvailable=true; 
+
+console.log(`${ProductName} price is = ${productPrice} with ${DiscountAmount}% Discount `);
---
+++ WebDev/JavaScript/01_BasicOfJs/VariableNaming.js
@@ -0,0 +1,6 @@
+// Variables Naming in Js
+
+let first_name="MD";
+let last_name="Alam";
+
+console.log(first_name + " " +last_name);
