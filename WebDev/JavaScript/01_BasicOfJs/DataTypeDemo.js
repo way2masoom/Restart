2ab938229265845c84f8d84db75dac9659e77669
way2masoom <way2masoom@gmail.com>
2025-07-29 21:30:18 +0545

    updated DataType

--- WebDev/JavaScript/01_BasicOfJs/DataTypeDemo.js
+++ WebDev/JavaScript/01_BasicOfJs/DataTypeDemo.js
@@ -17,3 +17,12 @@ console.log(x); // Undefined are done automatically by Js
 let y=null;
 console.log(y); // Null value should be define by the User/Developer
 
+// bigInt 
+let z=990000937798298762947829982300924390; // not a bigint
+console.log(z); 
+
+let n=990000937798298762947829982300924390n; // BigInt as the values have n at the last
+console.log(n); 
+
+
+
