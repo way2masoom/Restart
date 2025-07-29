6c1953b9e51d60e2df449c2f4c0b60ef788a4d44
way2masoom <way2masoom@gmail.com>
2025-07-29 21:38:14 +0545

    Assignment operator

---
+++ WebDev/JavaScript/01_BasicOfJs/AssignmentOperator.js
@@ -0,0 +1,20 @@
+// Assignment Operator 
+
+let a=30;
+let b=40;
+
+let sum=a+b; // plus + is the assignment operator
+console.log("Addition = "+sum);
+
+let sub=a-b;
+console.log("Subtraction = "+sub);
+
+let mult=a*b;
+console.log("multiply operator = "+mult);
+
+let divide=a/b;
+console.log("Divide operator = "+divide);
+
+let power=2**3;
+console.log("Power operator = "+power);
+
