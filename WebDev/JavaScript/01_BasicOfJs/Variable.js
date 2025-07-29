31eb61c55060ca027fb7f2733544cd2dbae2222f
way2masoom <way2masoom@gmail.com>
2025-07-29 10:37:03 +0545

    variable in js

---
+++ WebDev/JavaScript/01_BasicOfJs/Variable.js
@@ -0,0 +1,14 @@
+// Variable in js 
+// mainly have 3 types
+
+let name='alam';
+const age=21;
+
+console.log("Hi my name id "+name +"and my age is ="+age);
+
+var a=10;
+var b=20;
+
+let sum=a+b;
+
+console.log(`sum of a+b is = ${sum}`);
