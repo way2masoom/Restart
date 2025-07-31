2744b38d1147397059521447f6ce31845f5b6110
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2025-07-31 11:40:55 +0530

    updated

--- WebDev/JavaScript/01_BasicOfJs/VariableNaming.js
+++ WebDev/JavaScript/01_BasicOfJs/VariableNaming.js
@@ -9,3 +9,11 @@ console.log(first_name + " " +last_name);
 let iam$user='alam';
 console.log(user);
 
+// we can used number after the variable name not at begining 
+var user2=undefined;
+let user1='rohit'
+
+console.log(user1);
+console.log(user2);
+
+
