5d5966cf1c3695409a83ca77b68a4a182addb3b4
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2025-08-03 18:31:01 +0000

    while loop

---
+++ WebDev/JavaScript/05_Loops/whileDemo.js
@@ -0,0 +1,8 @@
+// Progrom to print numbers from 1 to 10 using a while loop
+
+let i=1; // Initialize the counter variable
+while(i<=10){
+    console.log(i);
+    i+=1; // Increment the counter variable
+}
+
