8aaa9ff975eb0d2579834c694171cbec6c6ece8a
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2025-08-03 18:07:43 +0000

    bitwise operators

---
+++ WebDev/JavaScript/02_Operators/BitwiseOperator.js
@@ -0,0 +1,22 @@
+// Bitwise Operators
+
+let a=5;
+let b=6;
+
+// bitwise and &
+console.log(a&b);
+
+// Bitwise or |
+console.log(a|4);
+
+// Bitwise xor ^
+console.log(a^b);
+
+// Bitwise not ~
+console.log(~a);
+
+// left shift <<
+console.log(a<<2);
+
+// right shift >>
+console.log(a>>2);
