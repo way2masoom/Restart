c2a085b7cf9102f9cd1306176b58c118c342bf59
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2025-08-03 18:40:28 +0000

    unery operators

---
+++ WebDev/JavaScript/05_Loops/UnaryOps.js
@@ -0,0 +1,13 @@
+// Unary operators in JavaScript
+
+let a=1;
+
+console.log(a++); // Postfix increment: prints 1, then a becomes 2
+console.log(a);   // Prints 2
+
+console.log();
+
+
+let b=1;
+console.log(++b); // Prefix increment: b becomes 2, then prints 2
+console.log(b);   // Prints 2
