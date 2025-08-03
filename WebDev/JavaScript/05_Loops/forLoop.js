dc87b3d0bb3a433d16808a3c7813971a82922caf
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2025-08-03 18:32:43 +0000

    for loop

---
+++ WebDev/JavaScript/05_Loops/forLoop.js
@@ -0,0 +1,12 @@
+// Program to demonstrate the use of a for loop in JavaScript
+
+for(let i = 0; i < 5; i++) {
+    console.log("Iteration number: " + i);
+}   
+
+console.log();
+
+// number from 1 to 10
+for(let i=1;i<=10;i++){
+    console.log("Number: " + i);
+}
