71c6fa0d63c0d87d9cb3541582a9164b3da38d5d
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2025-08-04 18:31:03 +0000

    basic function

---
+++ WebDev/JavaScript/06_Function/function1.js
@@ -0,0 +1,12 @@
+// basic of Function
+
+// Function decleration
+function greetingToEveryOne(){
+    console.log("My logic line 1");
+    console.log("My logic line 2");
+    console.log("My logic line 3");
+    
+}
+
+// Function call
+greetingToEveryOne();
