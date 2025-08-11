aeb6cabdfd2edc1fa1f745ff7841d810b41e814b
way2masoom <way2masoom@gmail.com>
2025-08-12 00:47:00 +0545

    function 4

---
+++ WebDev/JavaScript/06_Function/function3.js
@@ -0,0 +1,12 @@
+// Used of return in function
+
+function isEvenOrOdd(x){
+    if(x%2==0){
+        return "Even"
+    }else{
+        return "Odd"
+    }
+}
+
+let ans=isEvenOrOdd(10);
+console.log(ans);
