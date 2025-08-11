57d2e1e5b06c39012c76cd854dd237c79f76b826
way2masoom <way2masoom@gmail.com>
2025-08-12 00:11:52 +0545

    function 2(with parameters)

---
+++ WebDev/JavaScript/06_Function/function2.js
@@ -0,0 +1,12 @@
+// Example of function with parameters 
+
+function isEvenOrOdd(num){
+    if (num%2==0){
+        console.log("Even Number");
+    }else{
+        console.log("Odd Number");
+    }
+}
+
+let num=10;
+isEvenOrOdd(num);
