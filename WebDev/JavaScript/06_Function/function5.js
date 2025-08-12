5332e1329aa6700a3b7862f2f3d25a17300c80ad
way2masoom <way2masoom@gmail.com>
2025-08-12 11:25:40 +0545

    function with unlimited parameters

---
+++ WebDev/JavaScript/06_Function/function5.js
@@ -0,0 +1,10 @@
+// Function with unlimited Parameters
+
+function unLim(...value){
+    // ...value is rest parameters
+    console.log(value);
+}
+
+// passing unlimited value
+
+unLim(10,20,30,40,50,60);
