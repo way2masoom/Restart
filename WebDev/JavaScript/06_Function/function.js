4c7c6b50e562915c4b818f69c9f884ba2d69dec6
way2masoom <way2masoom@gmail.com>
2025-08-12 11:16:38 +0545

    function with default value

---
+++ WebDev/JavaScript/06_Function/function.js
@@ -0,0 +1,11 @@
+// Function Parameters with PreDefine
+
+function add(x,y=10){
+    return x+y;
+}
+
+console.log(add(10)); // if we don't pass any parameter then i will take Default value
+
+console.log(add(5,10)); // If we pass the value then with Take current value
+
+
