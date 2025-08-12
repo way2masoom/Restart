babe240cd59357ae68a7568354710f54e7953536
way2masoom <way2masoom@gmail.com>
2025-08-12 11:47:37 +0545

    InBuild Functions

---
+++ WebDev/JavaScript/06_Function/function6.js
@@ -0,0 +1,8 @@
+// In js allot of Build In Function Exist
+
+// Math.abs It convert negative value into positive
+console.log(Math.abs(-10));
+console.log(Math.abs(10)); 
+
+console.log(Math.sqrt(10));
+
