c7d51d900bf5b70167e4535115f29e6db7bbab98
way2masoom <way2masoom@gmail.com>
2025-08-12 11:22:11 +0545

    added function

--- WebDev/JavaScript/06_Function/function.js
+++ WebDev/JavaScript/06_Function/function.js
@@ -1,11 +1,26 @@
 // Function Parameters with PreDefine
 
-function add(x,y=10){
-    return x+y;
+function add(x, y = 10) {
+    console.log(x);
+    console.log(y);
+
+    return x + y;
 }
 
-console.log(add(10)); // if we don't pass any parameter then i will take Default value
+function add2(x, y = 10, z) {
+    console.log(x);
+    console.log(y);
+    console.log(z);
 
-console.log(add(5,10)); // If we pass the value then with Take current value
 
+    return x + y + z;
+}
+
+console.log("Function call : ",add(10)); // if we don't pass any parameter then i will take Default value
+console.log();
+
+console.log("Function call : ",add(5, 10)); // If we pass the value then with Take current value
+console.log();
+
+console.log("Function call : ",add2(5, 10, 7))
 
