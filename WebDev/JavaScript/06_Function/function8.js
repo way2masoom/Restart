70d253bd59ec711c03eca4a8f8478455f062a770
way2masoom <way2masoom@gmail.com>
2025-08-13 23:38:00 +0545

    date in function

---
+++ WebDev/JavaScript/06_Function/function8.js
@@ -0,0 +1,14 @@
+// Date Function or object 
+
+const d=new Date();
+console.log(d);
+
+// Date have many function 
+console.log("Today's Date = ",d.getDate()); // today's Date
+console.log("Today's Day = ",d.getDay());
+console.log("Today's Month = ",d.getMonth());
+console.log("Today's Year = ",d.getFullYear());
+console.log("Today's Hours = ",d.getHours());
+console.log("Today's Timezone Offset = ",d.getTimezoneOffset());
+
+
