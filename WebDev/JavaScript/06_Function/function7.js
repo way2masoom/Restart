add08f66202aa394e41a9cba9fbad2aa813fa635
way2masoom <way2masoom@gmail.com>
2025-08-13 23:29:04 +0545

    console log in function

---
+++ WebDev/JavaScript/06_Function/function7.js
@@ -0,0 +1,8 @@
+// console.log is also a function but no part of javascript 
+
+console.log("Hello this is md");
+console.log("And i am ",10," Old");
+
+console.log("Random text here to print in Console ");
+
+
