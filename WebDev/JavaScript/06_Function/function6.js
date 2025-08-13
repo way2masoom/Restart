86a963d0e8b6d8a35d6e1b1fa4f9f5ef7d4ecc3b
way2masoom <way2masoom@gmail.com>
2025-08-13 23:24:32 +0545

    function added

--- WebDev/JavaScript/06_Function/function6.js
+++ WebDev/JavaScript/06_Function/function6.js
@@ -4,8 +4,16 @@
 console.log(Math.abs(-10));
 console.log(Math.abs(10)); 
 
-console.log(Math.sqrt(10));
+console.log("Square of Number = ",Math.sqrt(10));
+console.log("Floor value = ",Math.floor(10.99));
+console.log("Floor Ceil = ",Math.ceil(10.99));
+console.log("Floor Round of = ",Math.round(10.99));
 
-console.log("minimum of two number", Math.min(10,2));
-console.log("Maximum of two number", Math.max(10,2));
+
+
+console.log("minimum of two number", Math.min(10,2,-2,10,11));
+console.log("Maximum of two number", Math.max(10,2,100,-1,22));
+
+console.log("Random Number ",Math.random()); // It doesn't take any parameter 
+console.log("Random Number in Single number ", Math.floor(Math.random()*100 + 1)); // It doesn't take any parameter 
 
