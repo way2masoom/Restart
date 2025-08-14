c4a4db9344c4c2632c8e8d62958797d4c4f75e37
way2masoom <way2masoom@gmail.com>
2025-08-15 02:00:09 +0545

    Basic Program in js

---
+++ WebDev/JavaScript/ProgramInJs/oddEvenNuber.js
@@ -0,0 +1,10 @@
+// Finding it the number is odd or even using if
+
+let num=6;
+
+// condition to find the number is odd or even
+if(num%2===0){
+    console.log(num+" is Even Number");
+}else{
+    console.log(num+" is Odd Number");
+}
