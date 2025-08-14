e7132009e1478f1886940188b96c48f4521745d0
way2masoom <way2masoom@gmail.com>
2025-08-15 01:37:57 +0545

    basic of array

--- WebDev/JavaScript/07_Arryas/arrays1.js
+++ WebDev/JavaScript/07_Arryas/arrays1.js
@@ -10,3 +10,14 @@ console.log(typeof arr2); // check the type of arr2
 console.log(Array.isArray(arr2)); //check if arr2 is an array
 console.log(arr2.length); // Length of the array
 
+console.log(arr[2]); // access element at index 2
+
+// updating the array Element 
+arr2[0]=-10;
+console.log(arr2)
+
+let arr3=new Array(10); // Create an array of length 10
+console.log(arr3);
+
+let arr4= new Array(5).fill(-3);
+console.log(arr4);
