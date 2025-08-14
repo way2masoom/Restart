5b2d0a1183d511f92e45d5b26c8ccda86272d2e0
way2masoom <way2masoom@gmail.com>
2025-08-14 13:09:45 +0545

    arrays

---
+++ WebDev/JavaScript/07_Arryas/arrays1.js
@@ -0,0 +1,12 @@
+// Crating arrays in JS
+
+let arr=[]; // Empty array
+let arr1=[1,2,3]; // Array with elements
+
+let arr2=[1,2,3,"Hello",true,null,-10,20,"alam"]; // Array with different types of elements
+console.log(arr2); // Print the array with different types of elements
+
+console.log(typeof arr2); // check the type of arr2
+console.log(Array.isArray(arr2)); //check if arr2 is an array
+console.log(arr2.length); // Length of the array
+
