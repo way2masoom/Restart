7e2be41e270af5b5732014a38cfb1b216369ca7d
way2masoom <way2masoom@gmail.com>
2025-08-15 16:34:23 +0545

    prime number

---
+++ WebDev/JavaScript/ProgramInJs/02_Loop/03_PrimeNumber.js
@@ -0,0 +1,25 @@
+// Check if the number is prime or not
+
+function isPrime(number) {
+    // Numbers less than 2 are not prime
+    if (number < 2) {
+        return false;
+    }
+
+    for (let i = 2; i < number - 1; i += 1) {
+        if (number % i == 0) {
+            return false;
+        } else {
+            return true
+        }
+    }
+}
+
+const PrimeNumber = isPrime(5);
+
+if (PrimeNumber == true) {
+    console.log("Yes it's prime number");
+} else {
+    console.log("NO it's a prime number");
+}
+
