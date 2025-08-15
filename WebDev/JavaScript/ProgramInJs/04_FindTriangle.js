79bb7462f317ed9e5313dedc2b12f2af048d6876
way2masoom <way2masoom@gmail.com>
2025-08-15 13:29:15 +0545

    formated the code

--- WebDev/JavaScript/ProgramInJs/04_FindTriangle.js
+++ WebDev/JavaScript/ProgramInJs/04_FindTriangle.js
@@ -1,13 +1,13 @@
 // Given a valid triangle a,b,c and find out 
 // The given triangle is Equilateral Triangle,Isosceles Triangle or Scalene Triangle
 
-function findTriangle(a,b,c){
+function findTriangle(a, b, c) {
     // condition for Equilateral Triangle
-    if(a===b && b===c && c===a){
+    if (a === b && b === c && c === a) {
         console.log("Is a Equilateral Triangle");
-    }else if(a===b || b===c || c===a){
+    } else if (a === b || b === c || c === a) {
         console.log("Is a Isosceles Triangle");
-    }else if(a!==b && b!==c && c!==a){
+    } else if (a !== b && b !== c && c !== a) {
         console.log("Is a Scalene Triangle");
     }
 }
@@ -15,7 +15,7 @@ function findTriangle(a,b,c){
 // Valid Triangle as Input
 // let a = 5, b = 6, c = 7;
 // let a = 5, b = 5, c = 5;
-let a=10,b=10,c=20;
+let a = 10, b = 10, c = 20;
 
 // calling the Function
-findTriangle(a,b,c);
+findTriangle(a, b, c);
