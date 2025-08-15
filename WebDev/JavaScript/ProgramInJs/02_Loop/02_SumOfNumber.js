d63303cb50a7184f618cc1fed36deb963d7333e0
way2masoom <way2masoom@gmail.com>
2025-08-15 15:32:49 +0545

    sum of Number(1 to 10)

---
+++ WebDev/JavaScript/ProgramInJs/02_Loop/02_SumOfNumber.js
@@ -0,0 +1,12 @@
+// Sum of Number using loop
+
+let sum=0;
+let i=1;
+
+while(i<=10){
+    sum+=i;
+    console.log(i);
+    i+=1;
+}
+console.log();
+console.log("The sum of first 10 natural Number  = "+sum);
