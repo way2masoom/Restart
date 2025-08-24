4e4241f586de937c55d81de4c8a2c208a2869543
way2masoom <way2masoom@gmail.com>
2025-08-24 21:23:47 +0545

    block Scopes

--- WebDev/JavaScript/08_Scopes/06_BlockScope.js
+++ WebDev/JavaScript/08_Scopes/06_BlockScope.js
@@ -2,5 +2,6 @@
     let message = "Hello from inside the block!";
     console.log(message); // Accessible here
 }
+let x=10;
 
 // console.log(message); // Uncaught ReferenceError: message is not defined
