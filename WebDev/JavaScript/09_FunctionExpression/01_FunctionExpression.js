07063ca46574502e6183f053e65db5e5183933f6
way2masoom <way2masoom@gmail.com>
2025-09-01 12:02:12 +0545

    Basic of function Expression

---
+++ WebDev/JavaScript/09_FunctionExpression/01_FunctionExpression.js
@@ -0,0 +1,18 @@
+// Basic Example of the Function Expression
+
+// FunctionExpression Declaration
+const myFun=function fun(){
+    console.log("Hello this is function expression");
+}
+
+// calling the function expression 
+myFun();
+
+
+// FunctionExpression Declaration with Parameters
+const myFun2=function gun(x){
+    console.log("Function with parameter X "+x);
+}
+
+// calling the function Expression with parameter
+myFun2(10);
