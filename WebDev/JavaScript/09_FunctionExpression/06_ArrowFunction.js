a8e28748670f21bd17b916c08c8a7c98944c95ce
way2masoom <way2masoom@gmail.com>
2025-09-02 16:03:19 +0545

    arrow function

---
+++ WebDev/JavaScript/09_FunctionExpression/06_ArrowFunction.js
@@ -0,0 +1,11 @@
+// Arrow function Examples
+
+// square of two number without returning it the value 
+let x = 4;
+const sqrt = (x) => x * x; // in arrow function we don't need to write return keyword it return the value automatically
+console.log("Square of the number is: ",sqrt(x));
+
+
+// Example of arrow function    
+const add=(a,b) => a+b; // arrow function with two parameter
+console.log("Addition of two number is: ",add(10,20));
