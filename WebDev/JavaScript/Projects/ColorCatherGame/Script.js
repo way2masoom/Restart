b4e64a0b678306515495ca571a5ad7cb1225fa92
way2masoom <way2masoom@gmail.com>
2025-10-11 10:09:19 +0545

    basic of Js Setup

--- WebDev/JavaScript/Projects/ColorCatherGame/Script.js
+++ WebDev/JavaScript/Projects/ColorCatherGame/Script.js
@@ -1,4 +1,27 @@
+// Global Variables
+let targetColor = "";
+let score = 0;
+let time = 30;
+let timer;
+let colors = ['red', 'green', 'blue', 'purple', 'hotpink', 'yellow', 'orange', 'pink', 'brown',
+    'cyan', 'magenta', 'lime', 'teal', 'navy', 'olive', 'maroon', 'gold'];
+
+const grid = document.getElementById("grid");
+const targetColorDisplay = document.getElementById("target-color");
+const timeDisplay = document.getElementById("time")
+const scoreDisplay = document.getElementById("score")
+
+// Function to Generate the random colors
+function getRandomColors() {
+
+}
+
 // Function to Start Game 
-function startGame(){
+function startGame() {
+    score = 0;
+    time = 30;
+    scoreDisplay.textContent = score;
+    timeDisplay.textContent = time;
     
+    createGrid();
 }
