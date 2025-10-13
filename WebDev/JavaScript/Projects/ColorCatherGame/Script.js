0c3dd53740fef3d1f4304e7caae21f699d9d56d4
way2masoom <way2masoom@gmail.com>
2025-10-13 09:16:56 +0545

    game complete

--- WebDev/JavaScript/Projects/ColorCatherGame/Script.js
+++ WebDev/JavaScript/Projects/ColorCatherGame/Script.js
@@ -3,40 +3,50 @@ let targetColor = "";
 let score = 0;
 let time = 30;
 let timer;
-let colors = ['red', 'green', 'blue', 'purple', 'hotpink', 'yellow', 'orange', 'pink', 'brown',
-    'cyan', 'magenta', 'lime', 'teal', 'navy', 'olive', 'maroon', 'gold'];
+let colors = ['red', 'green', 'blue', 'purple', 'hotpink', 'yellow', 'orange', 'pink', 'brown', 'magenta', 'lime', 'teal', 'navy', 'olive', 'maroon', 'gold'];
 
 const grid = document.getElementById("grid");
 const targetColorDisplay = document.getElementById("target-color");
 const timeDisplay = document.getElementById("time")
 const scoreDisplay = document.getElementById("score")
 
-// Function to Generate the random colors
-function getRandomColors() {
-
-}
 
 // ShuffleArray for colors to position Randomly 
 function shuffleArray(colors) {
-
+    for (let i = colors.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [colors[i], colors[j]] = [colors[j], colors[i]];
+    }
+    return colors;
 }
 
 
 // Grid Function to Arrange the color in Grid form
 function createGrid() {
     grid.innerHTML = "";
-    colors = shuffleArray(colors);
-    targetColor = colors[Math.floor(Math.random()) * 16];
+    let randomColors = shuffleArray([...colors]);
+    console.log(randomColors);
+
+    targetColor = colors[Math.floor(Math.random() * 16)];
     targetColorDisplay.textContent = targetColor;
-    colors.forEach((color) => {
+    randomColors.forEach((color) => {
         const box = document.createElement("div");
         box.className = "color-box";
         box.style.backgroundColor = color;
-        box.addEventListener('click', () => { 
-            handleClick(color); 
+        box.addEventListener('click', () => {
+            handleClick(color);
         })
         grid.appendChild(box);
     })
+}
+
+// Handel Click Function to Match the color with target color
+function handleClick(clickedColor) {
+    if (clickedColor === targetColor) {
+        score++;
+        scoreDisplay.textContent = score;
+        createGrid();
+    }
 }
 
 // Function to Start Game 
