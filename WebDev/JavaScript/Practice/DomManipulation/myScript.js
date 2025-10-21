a780912316a40e94549bb8762ea2307b53124ace
way2masoom <way2masoom@gmail.com>
2025-10-21 22:04:31 +0545

    DOMS Manipulation

---
+++ WebDev/JavaScript/Practice/DomManipulation/myScript.js
@@ -0,0 +1,14 @@
+console.log("Testing JS");
+
+let a=document.getElementById("heading1");
+// types of object we can used with document to change the text of html 
+// a.innerHTML="Heading change";
+// a.textContent="Hello world"
+// a.innerText="Heading change again"
+
+// Different between them 
+console.log(a.innerHTML);
+console.log(a.textContent);
+console.log(a.innerText);
+
+
