0499d29e7043b50c25b0c9551bc8dd237833fa11
way2masoom <way2masoom@gmail.com>
2025-10-26 07:36:35 +0545

    DarkMod and light Mod first way

---
+++ WebDev/JavaScript/Practice/EventManupulation/event.js
@@ -0,0 +1,10 @@
+
+//  Darkmod function 
+function DarkMod() {
+    document.body.style.backgroundColor = "#333";
+}
+
+// LightMod function
+function LightMod() {
+    document.body.style.backgroundColor = "#FFF";
+}
