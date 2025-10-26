f0bf39e681b4d1047b3320d8b8047e7fc2edacfa
way2masoom <way2masoom@gmail.com>
2025-10-26 07:44:29 +0545

    second way of Making DarkMod and LightMod

--- WebDev/JavaScript/Practice/EventManupulation/event.js
+++ WebDev/JavaScript/Practice/EventManupulation/event.js
@@ -1,10 +1,24 @@
+// First way 
 
-//  Darkmod function 
-function DarkMod() {
-    document.body.style.backgroundColor = "#333";
-}
+//  DarkMod function 
+// function DarkMod() {
+//     document.body.style.backgroundColor = "#333";
+// }
 
 // LightMod function
-function LightMod() {
-    document.body.style.backgroundColor = "#FFF";
+// function LightMod() {
+//     document.body.style.backgroundColor = "#FFF";
+// }
+
+// second way 
+
+let btn = document.getElementsByTagName("button");
+btn[0].addEventListener('click', DarkMod); // function passed in eventListener
+
+btn[1].addEventListener('click', () => {
+    document.body.style.backgroundColor = "#fff" }) // Made function here
+
+function DarkMod() {
+    document.body.style.backgroundColor = "#333"
 }
+
