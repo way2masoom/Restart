024b1c2cefda85505d39412ce76d744eda9e3927
way2masoom <way2masoom@gmail.com>
2025-11-09 10:39:08 +0545

    add Constructor demo and there basic used in JS

---
+++ WebDev/JavaScript/11_ObjectOrientedProgramming/02_Constructor.js
@@ -0,0 +1,42 @@
+// Constructor In js
+// constructor in js have implement by default with every calls we make 
+// but we can also create out own Constructor
+
+// Constructor demo
+class Product{
+    // defining the product 
+    name;
+    price;
+    description;
+    ratting;
+
+    // Members function
+    constructor(name,price,description,ratting){
+        this.name=name;
+        this.price=price;
+        this.description=description;
+        this.ratting=ratting;
+    }
+
+    // Buy the product
+    buyProduct(){
+        console.log("Buying the product");
+    }
+
+    addToCart(){
+        console.log("Adding to the Cart");
+        
+    }
+
+    removeCart(){
+        console.log("Removing the product from the cart");
+        
+    }
+
+}
+
+// Creating objects using constructor
+const product1=new Product("Iphone 16 Pro",120000,"Best Phone","5.2");
+console.log(product1);
+
+product1.addToCart();
