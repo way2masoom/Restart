997ac2ded40b2778549e62b6a3f1b49969870bb7
MD ALAM <alkaserphone@MacBook-Air.local>
2026-03-13 22:42:21 +0545

    ALL UPDATED

--- javaPratice/function/function4.js
+++
@@ -1,9 +0,0 @@
-// we can implement unlimited parameter
-
-function unlimited(...values) {
-    // ... parameter represent 'rest' parameter
-    console.log(values);
-}
-
-// calling the function 
-unlimited(10, 20, 30, 40, 50, 60, 70,"hello",true);
--- javaPratice/tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-function greeting() {
-    console.log("Hello good morning");
-}
