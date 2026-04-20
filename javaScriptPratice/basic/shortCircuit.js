d5c20a5b907c032d9ee0a672699f284c8fc72ac3
MD ALAM <alkaserphone@MacBook-Air.local>
2026-04-20 16:31:07 +0530

    Reviesed basic of JS

---
+++ javaScriptPratice/basic/shortCircuit.js
@@ -0,0 +1,16 @@
+let x = 30;
+let y = 40;
+
+console.log(x && y);
+console.log(y && x);
+console.log(x || y);
+console.log(y|| x);
+console.log(!x);
+console.log(!y);
+
+console.log(undefined&&null);
+
+
+
+
+
---
+++ javaScriptPratice/basic/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+function greeting() {
+    console.log("Hello good morning");
+}
